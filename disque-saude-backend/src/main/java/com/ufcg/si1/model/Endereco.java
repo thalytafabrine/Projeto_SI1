fe7c4ea86c41679c45441d750dfe6ab81405e609
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2017-08-07 17:22:27 -0300

    Hashcode e equals (Pessoa e endereco)

--- disque-saude-backend/src/main/java/com/ufcg/si1/model/Endereco.java
+++ disque-saude-backend/src/main/java/com/ufcg/si1/model/Endereco.java
@@ -38,5 +38,49 @@ public class Endereco {
 		this.cidade = cidade;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((rua == null) ? 0 : rua.hashCode());
+		result = prime * result + ((uf == null) ? 0 : uf.hashCode());
+		result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Endereco other = (Endereco) obj;
+
+		if (rua == null) {
+			if (other.rua != null)
+				return false;
+		} else if (!rua.equals(other.rua)) {
+			return false;
+		}
+		
+		if (uf == null) {
+			if (other.uf != null)
+				return false;
+		} else if (!uf.equals(other.uf)) {
+			return false;
+		}
+		
+		if (cidade == null) {
+			if (other.cidade != null)
+				return false;
+		} else if (!cidade.equals(other.cidade)) {
+			return false;
+		}
+		
+		return true;
+	}
 	
 }
