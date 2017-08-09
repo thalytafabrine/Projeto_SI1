dc8e52e6e455541f52a6d45e6fa2ece314a3369d
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2017-08-09 09:22:14 -0300

    Separando controllers do frontend

--- disque-saude-frontend/app/core/controller/complaintController.js
+++ disque-saude-frontend/app/core/controller/complaintController.js
@@ -12,18 +12,6 @@ app.controller("registerComplaintCtrl", function ($scope, $http, toastr, $locati
     }
 });
 
-app.controller("searchAverangeCtrl", function ($scope, $http) {
-
-    $scope.average = null;
-
-    $scope.searchAveragePerPatient = function (id) {
-        $http.get("http://localhost:5000/SpringBootRestApi/unidade/geral/medicos/" + id).then(function successCallback(response) {
-            $scope.average = response.data.obj;
-        }, function errorCallback(error) {
-            console.log("Unidade Não Encontrada");
-        });
-    }
-});
 
 app.controller("searchComplaintCtrl", function ($scope, $http) {
     $scope.complaint;
@@ -35,24 +23,6 @@ app.controller("searchComplaintCtrl", function ($scope, $http) {
             $scope.complaint = null;
             console.log(error);
         });
-    }
-});
-
-app.controller("searchHealthUnitCtrl", function ($scope, $http) {
-
-    $scope.units = [];
-
-    $scope.searchHU = function (neighborhood) {
-        $http.get("http://localhost:5000/SpringBootRestApi/unidade/busca?bairro=" + neighborhood)
-            .then(function success(response) {
-                $scope.units = [];
-                $scope.units.push(response.data);
-                console.log("Foram encontradas Unidades de saúde");
-                console.log(response.data);
-            }, function failed(error) {
-                console.log("Erro na busca de unidades");
-                console.log(error.data.errorMessage);
-            });
     }
 });
 
---
+++ disque-saude-frontend/app/core/controller/healthUnitController.js
@@ -0,0 +1,30 @@
+app.controller("searchHealthUnitCtrl", function ($scope, $http) {
+
+    $scope.units = [];
+
+    $scope.searchHU = function (neighborhood) {
+        $http.get("http://localhost:5000/SpringBootRestApi/unidade/busca?bairro=" + neighborhood)
+            .then(function success(response) {
+                $scope.units = [];
+                $scope.units.push(response.data);
+                console.log("Foram encontradas Unidades de saúde");
+                console.log(response.data);
+            }, function failed(error) {
+                console.log("Erro na busca de unidades");
+                console.log(error.data.errorMessage);
+            });
+    }
+});
+
+app.controller("searchAverangeCtrl", function ($scope, $http) {
+
+    $scope.average = null;
+
+    $scope.searchAveragePerPatient = function (id) {
+        $http.get("http://localhost:5000/SpringBootRestApi/unidade/geral/medicos/" + id).then(function successCallback(response) {
+            $scope.average = response.data.obj;
+        }, function errorCallback(error) {
+            console.log("Unidade Não Encontrada");
+        });
+    }
+});
