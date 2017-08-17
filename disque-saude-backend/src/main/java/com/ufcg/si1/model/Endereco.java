f20549aaac2f732e7572ea64f21cc3f89295fc5f
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2017-08-17 15:16:07 -0300

    transformando pessoa e endereco em entidades

--- disque-saude-backend/src/main/java/com/ufcg/si1/model/Endereco.java
+++ disque-saude-backend/src/main/java/com/ufcg/si1/model/Endereco.java
@@ -1,6 +1,15 @@
 package com.ufcg.si1.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Endereco {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
 
 	private String rua;
 
@@ -12,6 +21,14 @@ public class Endereco {
 		this.rua = rua;
 		this.uf = uf;
 		this.cidade = cidade;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getRua() {
@@ -37,51 +54,4 @@ public class Endereco {
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((rua == null) ? 0 : rua.hashCode());
-		result = prime * result + ((uf == null) ? 0 : uf.hashCode());
-		result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		
-		Endereco other = (Endereco) obj;
-
-		if (rua == null) {
-			if (other.rua != null)
-				return false;
-		} else if (!rua.equals(other.rua)) {
-			return false;
-		}
-		
-		if (uf == null) {
-			if (other.uf != null)
-				return false;
-		} else if (!uf.equals(other.uf)) {
-			return false;
-		}
-		
-		if (cidade == null) {
-			if (other.cidade != null)
-				return false;
-		} else if (!cidade.equals(other.cidade)) {
-			return false;
-		}
-		
-		return true;
-	}
-	
 }
--- disque-saude-backend/src/main/java/com/ufcg/si1/model/Pessoa.java
+++ disque-saude-backend/src/main/java/com/ufcg/si1/model/Pessoa.java
@@ -1,6 +1,15 @@
 package com.ufcg.si1.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa{
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
 	
 	private String nome;
 
@@ -12,6 +21,14 @@ public class Pessoa{
 		this.nome = nome;
 		this.email = email;
 		this.endereco = new Endereco(rua, uf, cidade);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getNome() {
