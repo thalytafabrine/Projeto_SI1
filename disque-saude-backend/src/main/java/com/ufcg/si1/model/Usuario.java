7b68bf27f3d259ae315734def4afcf25d4ad79e8
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2017-08-17 15:15:01 -0300

    Persistencia do usuario

--- disque-saude-backend/src/main/java/com/ufcg/si1/model/Usuario.java
+++ disque-saude-backend/src/main/java/com/ufcg/si1/model/Usuario.java
@@ -9,16 +9,16 @@ public class Usuario {
 
 	@Id
 	@GeneratedValue
-	private long id;
+	private Integer id;
 	private String nome;
 	private String email;
 	private String senha;
 	
-	public long getId() {
+	public Integer getId() {
 		return this.id;
 	}
 	
-	public void setId(long id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 	
@@ -46,47 +46,4 @@ public class Usuario {
 		this.senha = senha;
 	}
 
-	@Override
-	public int hashCode() {
-		
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		
-		Usuario other = (Usuario) obj;
-		
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (id != other.id)
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		if (senha == null) {
-			if (other.senha != null)
-				return false;
-		} else if (!senha.equals(other.senha))
-			return false;
-		return true;
-	}
 }
