9c7f0d58754ce5e6538ce5f18f9b4546692cb1b0
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2017-08-27 19:44:56 -0300

    Corrigindo funcoes de unidade de saude

--- disque-saude-frontend/app/core/controller/unidadeDeSaudeController.js
+++ disque-saude-frontend/app/core/controller/unidadeDeSaudeController.js
@@ -2,23 +2,22 @@ app.controller("unidadeDeSaudeCtrl", function ($scope, unidadeApi, $http) {
 
     $scope.unidades = [];
 
-    $scope.procuraHU = function (neighborhood) {
-        $http.get("http://localhost:5000/SpringBootRestApi/unidade/busca?bairro=" + neighborhood)
-            .then(function success(response) {
-                $scope.unidades = [];
-                $scope.unidades.push(response.data);
-                console.log("Foram encontradas Unidades de saúde");
-                console.log(response.data);
-            }, function failed(error) {
-                console.log("Erro na busca de unidades");
-                console.log(error.data.errorMessage);
-            });
+    $scope.procuraUS = function (bairro) {
+        unidadeApi.procuraUS(bairro).then(function success(response) {
+            $scope.unidades = [];
+            $scope.unidades.push(response.data);
+            console.log("Foram encontradas Unidades de saúde");
+            console.log(response.data);
+        }, function failed(error) {
+            console.log("Erro na busca de unidades");
+            console.log(error.data.errorMessage);
+        });
     }
 
     $scope.media = null;
 
-    $scope.pesquisaMediaPorPaciente = function (id) {
-        $http.get("http://localhost:5000/SpringBootRestApi/unidade/geral/medicos/" + id).then(function successCallback(response) {
+    $scope.pesquisaMediaMedicoPorPaciente = function (id) {
+        unidadeApi.pesquisaMediaMedicoPorPaciente(id).then(function successCallback(response) {
             $scope.media = response.data.obj;
         }, function errorCallback(error) {
             console.log("Unidade Não Encontrada");
--- disque-saude-frontend/app/core/service/unidadeService.js
+++ disque-saude-frontend/app/core/service/unidadeService.js
@@ -4,7 +4,17 @@ app.service("unidadeApi", function ($http, config) {
 		return $http.post(config.baseUrl + "/unidade/", unidade);
 	}
 
+	var _procuraUS = function (bairro) {
+		return $http.get(config.baseUrl + "/unidade/busca?bairro=" + bairro);
+	}
+
+	var _pesquisaMediaMedicoPorPaciente = function (id) {
+		return $http.get(config.baseUrl + "/unidade/geral/medicos/" + id);
+	}
+
 	return {
-		adicionarUnidadeDeSaude : _adicionarUnidadeDeSaude
+		adicionarUnidadeDeSaude : _adicionarUnidadeDeSaude,
+		procuraUS : _procuraUS,
+		pesquisaMediaMedicoPorPaciente : _pesquisaMediaMedicoPorPaciente
 	}
 });
